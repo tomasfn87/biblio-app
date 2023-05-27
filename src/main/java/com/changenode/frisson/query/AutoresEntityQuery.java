373b16aa717d920ff93758d7389ecd8bba8d835b
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2023-05-26 21:42:32 -0300

    Busca de autores (em andamento).

--- src/main/java/com/changenode/frisson/query/AutoresEntityQuery.java
+++ src/main/java/com/changenode/frisson/query/AutoresEntityQuery.java
@@ -3,5 +3,8 @@ package com.changenode.frisson.query;
 import com.changenode.frisson.model.Autores;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AutoresEntityQuery extends JpaRepository<Autores, Long> {
+    List<Autores> findByNomeOrSobrenomeOrDescricao(String nome, String sobrenome, String descricao);
 }
