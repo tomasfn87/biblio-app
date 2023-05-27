715ae53ff7c0d34816a424eb953b7514fbbee9e0
Saulo-Tadashi <tadashi.iguei@gmail.com>
2023-05-27 00:01:00 -0300

    funcao listarAutores com campo de busca funcionando

--- src/main/java/com/changenode/frisson/controller/ListarAutoresController.java
+++ src/main/java/com/changenode/frisson/controller/ListarAutoresController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -19,32 +19,23 @@ public class ListarAutoresController {
     @Autowired
     AutoresEntityQuery autoresEntityQuery;
 
-    @ModelAttribute("listaAutores")
     public List<Autores> obterListaDeAutores() {
         return autoresEntityQuery.findAll();
     }
 
-    @ModelAttribute("buscaAutores")
-    public List<Autores> obterBuscaPorAutor(@RequestParam(value = "termo", required = false) String termo) {
+    public List<Autores> obterBuscaPorAutor(String termo) {
         return autoresEntityQuery.findByNomeOrSobrenomeOrDescricao(termo, termo, termo);
     }
 
     @RequestMapping(value = "/public/listarAutores", method = GET)
     public String listarAutores(Model model) {
-        List<Autores> listaDeAutores = obterListaDeAutores();
-        model.addAttribute("listaAutores", listaDeAutores);
+        model.addAttribute("listaAutores", obterListaDeAutores());
         return "templates/listarAutores";
     }
 
-    @RequestMapping(value = "/public/buscarAutores", method = POST)
-    public String buscarAutores(@ModelAttribute(name="termo") String termo, Model model) {
-        List<Autores> resultadoBusca;
-        if (termo != null && !termo.isEmpty()) {
-            resultadoBusca = obterBuscaPorAutor(termo);
-        } else {
-            resultadoBusca = Collections.emptyList(); // Trazer uma lista vazia caso não exista uma valor para 'termo'
-        }
-        model.addAttribute("buscaAutores", resultadoBusca);
-        return "templates/buscarAutores";
+    @RequestMapping(value = "/public/listarAutores", method = POST)
+    public String buscarAutores(HttpServletRequest request, Model model) {
+        model.addAttribute("listaAutores", obterBuscaPorAutor(request.getParameter("termo")));
+        return "templates/listarAutores";
     }
 }
